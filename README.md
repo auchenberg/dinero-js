5e21ac50ff2bbe6611f049aa91a9cc4d663016c1
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-07-04 11:47:30 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,46 @@
+# dinero-js
+API client for [Dinero Regnskab](http://dinero.dk)
+
+Requires `client_id` + `client_secret` from [api.dinero.dk](http://api.dinero.dk)
+
+#### Example
+
+```javascript
+var fs = require('fs')
+var Dinero = require('dinero')
+var moment = require('moment')
+
+var client = new Dinero({
+  clientId: '<your client id>',
+  clientSecret: '<your client secret>'
+})
+
+var apiKey = '<your organiztion API key>'
+var orgId = '<your organization id>
+
+client.auth(apiKey, apiKey).then(function(auth) {
+  console.log('.. authenticated!')
+
+  var createFile = client.files.create(orgId, {
+    image: fs.createReadStream(__dirname + '/test1.pdf')
+  }, { 
+    multipart: true
+  })
+
+  createFile.then(function(body) {
+    console.log('... file uploaded, id=', body.FileGuid)    
+    return client.vouchers.purchase.create(orgId, {
+      FileGuid: body.FileGuid,
+      Notes: 'Uploaded from email',
+      VoucherDate: moment(new Date()).format('YYYY-DD-MM')
+    })
+  }).then(function(body){
+    console.log('.... voucher created, id=', body.VoucherGuid)
+    console.log('DONE')
+  }).catch(function(err) {
+    console.log('error', err)
+  })
+
+})
+
+```
---
+++ index.js
@@ -0,0 +1,152 @@
+'use strict'
+
+var request = require('request')
+var Promise = require('es6-promise').Promise
+
+var actions = {
+  'delete': 'del',
+  'get': 'get',
+  'post': 'create',
+  'put': 'update'
+}
+
+var endpoints = [
+  'post /:id/files',
+  'post /:id/vouchers/purchase',
+]
+
+function Dinero (options) {
+
+  this.clientId = options.clientId,
+  this.clientSecret = options.clientSecret,
+  this.accessToken = ''
+  this.authEndpoint = 'https://authz.dinero.dk/dineroapi/oauth/token'
+
+  this.settings = {
+    url: 'https://api.dinero.dk/v1',
+    params: {
+      access_token: this.accessToken
+    },
+    headers: {}
+  }
+
+  if (typeof options === 'object') {
+    extend(this.settings, options)
+  }
+
+  initialize.call(this)
+}
+
+Dinero.prototype = {
+
+  auth: function(username, password) {
+
+    return new Promise(function (resolve, reject) {
+
+      var options = {
+        url: this.authEndpoint,
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic ' + new Buffer(this.clientId + ':' + this.clientSecret).toString('base64'),
+        },
+        form: {
+            'grant_type' : 'password',
+            'scope': 'read write',
+            'username': username,
+            'password': password,
+        },         
+        json: true
+      }
+
+      request(options, function (err, response) {
+        if(err) {
+          reject(err)
+        } else {
+          this.accessToken = response.body.access_token
+          this.settings.headers = {
+            'Authorization': 'Bearer ' + this.accessToken
+          }
+          resolve(response.body)
+        }
+      }.bind(this))
+    }.bind(this))
+
+  },
+
+  request: function (endpoint, method, data, options) {
+    return new Promise(function (resolve, reject) {
+      var req = {
+        url: this.settings.url + endpoint,
+        method: method.toUpperCase(),
+        headers: this.settings.headers,
+        json: data,
+      }
+         
+      if (typeof options === 'object') {
+        if(options.multipart) { // Used for file uploading
+          req.json = true
+          req.formData = data
+        }
+      }
+
+      return request(req, function (err, response) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(response.body)
+        }
+      })
+
+    }.bind(this))
+
+  }
+
+}
+
+function initialize () {
+  function createNestedObject (base, names, value) {
+    var lastName = arguments.length === 3 ? names.pop() : false
+    for (var i = 0; i < names.length; i++) {
+      if(names[i] !== '') {     
+        base = base[names[i]] = base[names[i]] || {}
+      }
+    }
+    if (lastName) {
+      base = base[lastName] = value
+    }
+    return base
+  }
+
+  function addMethod (route) {
+    var parts = route.split(' ')
+    var method = parts[0]
+    var path = parts[1]
+    var pathArr = parts[1].replace('/:id', '').split('/')
+
+    pathArr.push(actions[method])
+
+    var fn = function () {
+      var args = Array.prototype.slice.call(arguments)
+      var endpoint = path
+      if (path.indexOf(':id') > -1) {
+        endpoint = path.replace(':id', args[0])
+        args.shift()
+      }
+      return this.request(endpoint, method, args[0], args[1])
+    }.bind(this)
+
+    createNestedObject(this, pathArr, fn)
+  }
+
+  for (var i = 0; i < endpoints.length; i += 1) {
+    addMethod.call(this, endpoints[i])
+  }
+}
+
+function extend (target, obj) {
+  for (var key in obj) {
+    target[key] = obj[key]
+  }
+}
+
+module.exports = Dinero
